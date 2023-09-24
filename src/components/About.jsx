6ccee67a0d98a4ad123faaa61535c33b48589a84
Paul Yi <pauljsyi@gmail.com>
2023-09-24 03:16:43 -0600

    set radius to 300

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,12 @@ const About = () => {
   }, []);
 
   return (
-    <motion.div
-      className={`${isFS ? "" : "grid grid-cols-3 gap-4"} mx-5 snap-start`}
-    >
+    <motion.div className={`${isFS ? "" : "grid grid-cols-3 gap-4"} mx-5 `}>
       <motion.div className="mt-20 col-span-1" variants={textVariant()}>
         <p className={`${styles.sectionSubText} flex`}>About Me</p>
       </motion.div>
       <motion.p
-        className="mt-20 col-span-2 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className="mt-20 col-span-2  text-[17px] max-w-3xl leading-[30px]"
         variants={fadeIn("", "", 0.1, 1)}
       >
         Hi, my name is Paul Yi. I am a Full Stack Developer with experience in
--- src/components/TechCloud.jsx
+++ src/components/TechCloud.jsx
@@ -1,58 +1,58 @@
-import { useEffect } from "react";
+import { useLayoutEffect, useState, useMemo } from "react";
 // import "../styles/TextSphere.css"
 import TagCloud from "TagCloud";
 import { styles } from "../styles";
 
 const TechCloud = () => {
-  useEffect(() => {
-    return () => {
-      const container = ".tagcloud";
-      const techs = [
-        "HTML",
-        "CSS",
-        "SASS",
-        "JavaScript",
-        "TailwindCSS",
-        "React",
-        "Vue",
-        "Nest.js",
-        "Node.js",
-        "Python",
-        "Flask",
-        "Jinja",
-        "MySQL",
-        "PostgreSQL",
-        "MongoDB",
-        "C#",
-        ".NET",
-        "Entity Framework",
-        "Mongoose",
-        "Sequelize",
-        "Git",
-        "Three.js",
-        "Blender",
-        "ChakraUI",
-        "Bootstrap",
-        "Realm",
-        "Postman",
-        "Express",
-        "Redux",
-      ];
-      const options = {
-        radius: 300,
-        maxSpeed: "normal",
-        initSpeed: "normal",
-        keep: true,
-      };
-      TagCloud(container, techs, options);
+  useLayoutEffect(() => {
+    const container = ".tagcloud";
+    const techs = [
+      "HTML",
+      "CSS",
+      "SASS",
+      "JavaScript",
+      "TailwindCSS",
+      "React",
+      "Vue",
+      "Nest.js",
+      "Node.js",
+      "Python",
+      "Flask",
+      "Jinja",
+      "MySQL",
+      "PostgreSQL",
+      "MongoDB",
+      "C#",
+      ".NET",
+      "Entity Framework",
+      "Mongoose",
+      "Sequelize",
+      "Git",
+      "Three.js",
+      "Blender",
+      "ChakraUI",
+      "Bootstrap",
+      "Realm",
+      "Postman",
+      "Express",
+      "Redux",
+      "GSAP",
+    ];
+    const options = {
+      radius: 300,
+      maxSpeed: "normal",
+      initSpeed: "normal",
+      keep: false,
     };
-  }, [1]);
+
+    return () => TagCloud(container, techs, options);
+  }, []);
   return (
-    <div className="flex justify-center  ">
-      <h1 className={`${styles.heroSubText} flex-col`}>
-        What I have worked with
-      </h1>
+    <div className="flex justify-center mt-20 ">
       <div className={`text-sphere ${styles.sectionSubText}`}>
+        <h1 className={`${styles.heroSubText} text-center`}>
+          What I have worked with
+        </h1>
         {/* span tag with classname tagcloud to work */}
         <span className="tagcloud"></span>
       </div>
